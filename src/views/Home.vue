c78d4ccb6c532c12053ee841cab85b5e3a245b6d
한건 <ofliberty@naver.com>
2021-05-20 14:30:19 +0900

    home tmdb

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>TMDB TEST</h1>
+    <!-- <button @click="getResult">popular</button> -->
+    <div v-for='result in results' :key='result.id'>
+      <p>{{result.title}}</p>
+      <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='300px'>
+    </div>
+    <!-- <input type='text' v-model="query" @keyup="getResult(query)"> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+      // query:  '',
+      results: '',
+    }
+  },
+  methods: {
+    getResult: function () {
+      axios({
+        method: 'get',
+        url: 'https://api.themoviedb.org/3/movie/popular?api_key=3ba0ab6eaf6d388c9d40f9c95a9fcdf1&language=ko-KR&page=1'
+      }).then(res => {
+        console.log(res)
+        this.results = res.data.results
+      })
+    }
+  },
+  created: function () {
+    this.getResult()
+  },
 }
 </script>
