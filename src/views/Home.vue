a5f3f4bfaf0c75c09eb20e4c4ba0ad91849459ec
김준영 <wns312@gmail.com>
2021-05-26 11:58:06 +0900

    css start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-<div class='container-fluid row'>
-    <h1 class='col'>Home입니다. 아직 안만들었어요</h1>
+<div class=''>
+    <h1 class=''>Home입니다. 아직 안만들었어요</h1>
+    <div class="container-sm">100% wide until small breakpoint</div>
+    <div class="container-md">100% wide until medium breakpoint</div>
+    <div class="container-lg">100% wide until large breakpoint</div>
+    <div class="container-xl">100% wide until extra large breakpoint</div>
+    <div class="container-xxl">100% wide until extra extra large breakpoint</div>
+    <div class="container-fluid">fluid</div>
 </div>
 </template>
 
@@ -9,3 +15,23 @@ export default {
     name : 'Home'
 }
 </script>
+<style scoped>
+.container-sm {
+    background-color: aqua;
+}
+.container-md {
+    background-color: crimson;
+}
+.container-lg {
+    background-color: coral;
+}
+.container-xl {
+    background-color: cornflowerblue;
+}
+.container-xxl {
+    background-color: chartreuse;
+}
+.container-fluid {
+    background-color: grey;
+}
+</style>
